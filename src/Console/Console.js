dbcc28a19d033974e552c62b486cb6dd356104d3
Leonid <leomrg861@gmail.com>
2021-08-20 22:36:43 +0300

    handle delete

--- src/Console/Console.js
+++ src/Console/Console.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 import PropTypes from "prop-types";
 
 export default class Console extends Component {
@@ -6,8 +6,9 @@ export default class Console extends Component {
     super(props);
     this.state = {
       enteredText: "",
-      newText: "",
+      command: "",
     };
+    this.ref = createRef();
   }
 
   static propTypes = {
@@ -24,37 +25,108 @@ export default class Console extends Component {
 
   onKeyPress = (e) => {
     if (e.key === "Enter") {
-      const { newText, enteredText } = this.state;
-      let commandOutput = "";
-      if (this.props.commandHandler) {
-        commandOutput = this.props.commandHandler(newText);
-      }
-      this.setState({
-        enteredText:
-          enteredText +
-          this.props.promptString +
-          newText +
-          "\n" +
-          commandOutput +
-          "\n",
-        newText: "",
-      });
+      this.handleEnterKey();
     } else {
-      this.setState({ newText: this.state.newText + e.key });
+      e.preventDefault();
+      const { selectionStart, selectionEnd } = e.target;
+      const cmd = this.state.command;
+      let { start, end } = this.getSelectionInCommand(
+        selectionStart,
+        selectionEnd
+      );
+      if (end < 0) return;
+      if (start < 0) start = 0;
+      const newCommand = cmd.substring(0, start) + e.key + cmd.substring(end);
+      this.setState({ command: newCommand }, () => {
+        this.ref.current.selectionStart = this.ref.current.selectionEnd =
+          selectionStart + 1;
+      });
     }
   };
 
+  onKeyDown = (e) => {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+      case 8: //backspace
+        break;
+      case 9: //tab
+        break;
+      case 46: //delete
+        this.handleDeleteKey(e);
+        break;
+      default:
+        break;
+    }
+  };
+
+  getSelectionInCommand(selectionStart, selectionEnd) {
+    const staticTextLength =
+      this.state.enteredText.length + this.props.promptString.length;
+    const start = selectionStart - staticTextLength;
+    const end = selectionEnd - staticTextLength;
+    return { start, end };
+  }
+
+  handleDeleteKey(e) {
+    e.preventDefault();
+    const { selectionStart, selectionEnd } = e.target;
+    const { command, enteredText } = this.state;
+
+    // calculate start and end in the command string
+    const staticTextLength =
+      enteredText.length + this.props.promptString.length;
+    let { start, end } = this.getSelectionInCommand(
+      selectionStart,
+      selectionEnd
+    );
+    if (end < 0) return;
+    if (start < 0) start = 0;
+
+    let newCommand; // after delete
+    if (end > start) {
+      newCommand = command.substring(0, start) + command.substring(end);
+    } else {
+      newCommand = command.substring(0, start) + command.substring(end + 1);
+    }
+    let newSelectionStart =
+      selectionStart < staticTextLength ? staticTextLength : selectionStart;
+
+    this.setState({ command: newCommand }, () => {
+      this.ref.current.selectionStart = this.ref.current.selectionEnd = newSelectionStart;
+    });
+  }
+
+  handleEnterKey() {
+    const { command, enteredText } = this.state;
+    let commandOutput = "";
+    if (this.props.commandHandler) {
+      commandOutput = this.props.commandHandler(command);
+    }
+    this.setState({
+      enteredText:
+        enteredText +
+        this.props.promptString +
+        command +
+        "\n" +
+        commandOutput +
+        "\n",
+      command: "",
+    });
+  }
+
   render() {
-    const { newText, enteredText } = this.state;
+    const { command, enteredText } = this.state;
     return (
       <div>
         <textarea
+          ref={this.ref}
           style={{
             width: "800px",
             height: "400px",
           }}
-          value={enteredText + this.props.promptString + newText}
+          value={enteredText + this.props.promptString + command}
           onKeyPress={this.onKeyPress}
+          onKeyDown={this.onKeyDown}
         />
       </div>
     );
