fda9182f2960eb5aabf6b400ae927a68b9112d4f
Leonid <leomrg861@gmail.com>
2021-08-20 19:14:04 +0300

    console rough logic

--- src/Console/Console.js
+++ src/Console/Console.js
@@ -1,24 +1,61 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Console extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      allText: ">",
+      enteredText: "",
+      newText: "",
     };
   }
 
+  static propTypes = {
+    promptString: PropTypes.string,
+    commandHandler: PropTypes.func,
+  };
+
+  static defaultProps = {
+    promptString: ">",
+    commandHandler: (cmd) => "executed",
+  };
+
   componentDidMount() {}
 
-  onChange = (e) => {
-    const newText = e.target.value;
-    this.setState({ allText: newText });
+  onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      const { newText, enteredText } = this.state;
+      let commandOutput = "";
+      if (this.props.commandHandler) {
+        commandOutput = this.props.commandHandler(newText);
+      }
+      this.setState({
+        enteredText:
+          enteredText +
+          this.props.promptString +
+          newText +
+          "\n" +
+          commandOutput +
+          "\n",
+        newText: "",
+      });
+    } else {
+      this.setState({ newText: this.state.newText + e.key });
+    }
   };
 
   render() {
+    const { newText, enteredText } = this.state;
     return (
       <div>
-        <textarea value={this.state.allText} onChange={this.onChange} />
+        <textarea
+          style={{
+            width: "800px",
+            height: "400px",
+          }}
+          value={enteredText + this.props.promptString + newText}
+          onKeyPress={this.onKeyPress}
+        />
       </div>
     );
   }
