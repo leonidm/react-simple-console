19347d3457cf38f455d10bfc9f57daad86ea1db6
Leonid <leomrg861@gmail.com>
2021-08-22 19:48:55 +0300

    copy paste

--- src/Console/Console.js
+++ src/Console/Console.js
@@ -11,29 +11,7 @@ export default class Console extends Component {
     this.ref = createRef();
   }
 
-  static propTypes = {
-    promptString: PropTypes.string,
-    commandHandler: PropTypes.func,
-    background: PropTypes.string,
-    fontColor: PropTypes.string,
-    width: PropTypes.number,
-    height: PropTypes.number,
-  };
-
-  static defaultProps = {
-    promptString: ">",
-    commandHandler: (cmd) => "executed",
-    background: "black",
-    fontColor: "white",
-    width: 600,
-    height: 300,
-  };
-
   componentDidMount() {
-    /*this.ref.current.addEventListener("select", (x) => {
-      console.log("select", x);
-    });*/
-
     this.ref.current.addEventListener("keyup", this.ensureValidCaretPosition.bind(this));
     this.ref.current.addEventListener("click", this.ensureValidCaretPosition.bind(this));
   }
@@ -56,7 +34,6 @@ export default class Console extends Component {
   };
 
   onKeyDown = (e) => {
-    console.log(e.keyCode);
     switch (e.keyCode) {
       case 8: //backspace
         this.handleBackspaceKey(e);
@@ -69,22 +46,64 @@ export default class Console extends Component {
       default:
         break;
     }
+    if (e.ctrlKey && !e.altKey && !e.shiftKey) {
+      switch (e.key) {
+        case "c":
+          this.copySelectionToClipboard();
+          break;
+        case "v":
+          //console.log(e.target.selectionStart, e.target.selectionEnd);
+          this.pasteSelectionFromClipboard();
+          break;
+        default:
+          break;
+      }
+    }
   };
+
+  copySelectionToClipboard() {
+    const { selectionStart, selectionEnd, value } = this.ref.current;
+    if (selectionStart < selectionEnd) {
+      const selectedText = value.slice(selectionStart, selectionEnd);
+      navigator.clipboard.writeText(selectedText);
+    }
+  }
+
+  pasteSelectionFromClipboard() {
+    const { selectionStart, selectionEnd } = this.ref.current;
+    navigator.clipboard.readText().then((textFromClipboard) => {
+      textFromClipboard = textFromClipboard.replace('\r', '');
+      const { command } = this.state;
+      let { start, end } = this.getSelectionInCommand(selectionStart, selectionEnd);
+      if (end < 0) return;
+      if (start < 0) start = 0;
+      const newCommand = command.substring(0, start) + textFromClipboard + command.substring(end);
+
+      this.setState({ command: newCommand }, () => {
+        this.ref.current.selectionStart = this.ref.current.selectionEnd =
+          this.getStaticTextLength() + start + textFromClipboard.length;
+      });
+    });
+  }
 
   ensureValidCaretPosition(e) {
     // check if there is no selection and caret position is too small set it to minimal valid
     const { selectionStart, selectionEnd } = e.target;
     const noSelection = selectionEnd === selectionStart;
     if (noSelection) {
-      const minimalValidPosition = this.state.enteredText.length + this.props.promptString.length;
+      const minimalValidPosition = this.getStaticTextLength();
       if (selectionStart < minimalValidPosition) {
         e.target.selectionStart = e.target.selectionEnd = minimalValidPosition;
       }
     }
   }
 
+  getStaticTextLength() {
+    return this.state.enteredText.length + this.props.promptString.length;
+  }
+
   getSelectionInCommand(selectionStart, selectionEnd) {
-    const staticTextLength = this.state.enteredText.length + this.props.promptString.length;
+    const staticTextLength = this.getStaticTextLength();
     const start = selectionStart - staticTextLength;
     const end = selectionEnd - staticTextLength;
     return { start, end };
@@ -93,10 +112,10 @@ export default class Console extends Component {
   handleDeleteKey(e) {
     e.preventDefault();
     const { selectionStart, selectionEnd } = e.target;
-    const { command, enteredText } = this.state;
+    const { command } = this.state;
 
     // calculate start and end in the command string
-    const staticTextLength = enteredText.length + this.props.promptString.length;
+    const staticTextLength = this.getStaticTextLength();
     let { start, end } = this.getSelectionInCommand(selectionStart, selectionEnd);
     if (end < 0) return;
     if (start < 0) start = 0;
@@ -118,10 +137,10 @@ export default class Console extends Component {
   handleBackspaceKey(e) {
     e.preventDefault();
     const { selectionStart, selectionEnd } = e.target;
-    const { command, enteredText } = this.state;
+    const { command } = this.state;
 
     // calculate start and end in the command string
-    const staticTextLength = enteredText.length + this.props.promptString.length;
+    const staticTextLength = this.getStaticTextLength();
     let { start, end } = this.getSelectionInCommand(selectionStart, selectionEnd);
     if (end < 0) return;
     if (start < 0) start = 0;
@@ -163,7 +182,8 @@ export default class Console extends Component {
     const { command, enteredText } = this.state;
     const { height, width, background, fontColor, promptString } = this.props;
     return (
-      <textarea spellcheck="false"
+      <textarea
+        spellCheck="false"
         ref={this.ref}
         style={{
           width: width + "px",
@@ -174,7 +194,26 @@ export default class Console extends Component {
         value={enteredText + promptString + command}
         onKeyPress={this.onKeyPress}
         onKeyDown={this.onKeyDown}
+        onChange={() => {}}
       />
     );
   }
 }
+
+Console.propTypes = {
+  promptString: PropTypes.string,
+  commandHandler: PropTypes.func,
+  background: PropTypes.string,
+  fontColor: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+Console.defaultProps = {
+  promptString: ">",
+  commandHandler: (cmd) => "executed",
+  background: "black",
+  fontColor: "white",
+  width: 600,
+  height: 300,
+};
